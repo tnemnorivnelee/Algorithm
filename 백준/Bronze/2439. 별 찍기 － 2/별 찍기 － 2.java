0635cc0c2e5ae128ed5a62aee9793a1b70bfa09b
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2024-10-09 17:04:17 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 224 ms, Memory: 19936 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        // 잘 이해 못함
+        int i = 0;
+        while(i++ < N) {
+            for(int j = 0; j < N; j++) {
+                if(j < N - i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
